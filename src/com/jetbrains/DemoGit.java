45c39795c3afc66a8cf55f3d55e6080a325815db
jiazhengsong <jis169@ucsd.edu>
2019-07-02 00:24:53 -0700

    Add Javadoc

---
+++ src/com/jetbrains/DemoGit.java
@@ -0,0 +1,18 @@
+package com.jetbrains;
+/**
+ * Simple class to test submitting to GitHub repository.
+ * @author Jiazheng Song
+ */
+public class DemoGit
+{
+
+    /**
+     * Outputs my name
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello. My name is JiazhengSong");
+
+    }
+
+}
